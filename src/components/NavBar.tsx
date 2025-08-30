b2edf6f2f91efc466f4d15d2859c34035e0401e8
Tushar Tyagi <tushartyagi@Tushars-MacBook-Pro.local>
2025-08-30 20:53:12 +0530

    enabled dark light mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/Logo/logo.webp'
-
+import { ColorModeButton } from "./ui/color-mode"
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize='60px'/>
+    <HStack justifyContent='space-between' padding="10px">
+        <Image src={logo} boxSize='60px' />
+         <ColorModeButton />
     </HStack>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.tsx";
+import { ColorModeProvider } from "./components/ui/color-mode"
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <Provider >
+      <ColorModeProvider>
       <App />
+      </ColorModeProvider>
     </Provider>
   </StrictMode>
 );
