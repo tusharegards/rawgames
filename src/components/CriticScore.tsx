d134373335f3a82046709f509d6db1351590accb
Tushar Tyagi <tushartyagi@Tushars-MacBook-Pro.local>
2025-09-01 14:12:21 +0530

    displaying criticscore on badge

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import { Badge } from '@chakra-ui/react';
+import React from 'react'
+
+interface Props {
+    score : number;
+}
+
+const CriticScore = ({score} : Props) => {
+    const color = score > 90 ? 'green' : score > 85 ? 'yellow' : 'red';
+  return (
+    <Badge colorPalette={color} fontSize='14px' paddingX={2} borderRadius={2}>{score}</Badge>
+  )
+}
+
+export default CriticScore
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -22,7 +22,7 @@ const PlatformIconList = ({platform}: Props) => {
     }
   return (
     <HStack marginY={1}>
-        {platform.map((platform) => <Icon as={iconMap[platform.slug]} color='gray.500'/>)}
+        {platform.map((platform) => <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>)}
     </HStack>
   )
 }
